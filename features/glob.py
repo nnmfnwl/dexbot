79da6038f6289f4414b8ffc3778fcfe16fc0fa83
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-10-25 22:15:23 +0000

    upt glob.py - add glob.t.argparse_bool fn

--- features/glob.py
+++ features/glob.py
@@ -12,5 +12,21 @@ def __init__():
     #global dynamic variables
     global d
     d = GlobVars()
+    #global tool functions
+    global t
+    t = GlobVars()
+    #init
+    t.argparse_bool = argparse_bool
+
+def argparse_bool(arg):
+    if isinstance(arg, bool):
+        return arg
+    elif isinstance(arg, str):
+        if arg.lower() in ['true', 'enabled', 'yes', '1']:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 __init__()
