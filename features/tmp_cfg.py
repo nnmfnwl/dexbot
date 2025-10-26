4b694a289d6448d03fd51200f9b4263443dfc125
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-10-26 11:03:02 +0000

    upt tmp_cfg.py - update temporary configuration according to style of other features

--- features/tmp_cfg.py
+++ features/tmp_cfg.py
@@ -4,16 +4,52 @@
 
 import time
 import pickle
+import sys
 
 import features.glob as glob
 
-def feature__tmp_cfg__load_cfg(tmp_cfg_file_name):
+# preconfiguration  initialization
+def feature__tmp_cfg__init_preconfig__():
     
-    glob.d.feature__tmp_cfg__file_name = tmp_cfg_file_name
+    glob.c.feature__tmp_cfg__file_name = None
     glob.d.feature__tmp_cfg__data = {}
+    glob.d.feature__tmp_cfg__ready = False
+
+feature__tmp_cfg__init_preconfig__()
+
+# define argument parameter
+def feature__tmp_cfg__load_config_define(parser, argparse):
+    return
+
+# parse configuration value
+def feature__tmp_cfg__load_config_postparse(args):
+    
+    glob.c.feature__tmp_cfg__file_name = str(args.config)
+
+# verify argument value after load
+def feature__tmp_cfg__load_config_verify():
+    
+    error_num = 0
+    crazy_num = 0
+    
+    if glob.c.feature__tmp_cfg__file_name != None and glob.c.feature__tmp_cfg__file_name != "":
+        glob.c.feature__tmp_cfg__file_name = glob.c.feature__tmp_cfg__file_name + ".tmp.cfg"
+        glob.d.feature__tmp_cfg__ready = True
+    else:
+        print('** ERROR >> setting >> --config filename+.tmp.cfg >> value <{}> is invalid'.format(glob.c.feature__tmp_cfg__file_name))
+        error_num += 1
+        
+    return error_num, crazy_num
+
+# load temporary configuration from saved file
+def feature__tmp_cfg__load_saved_cfg():
+    
+    if glob.d.feature__tmp_cfg__ready == False:
+        print('** FATAL >> feature__tmp_cfg >> must be initialized first!')
+        sys.exit(1)
     
     try:
-        file = open(glob.d.feature__tmp_cfg__file_name, "rb")
+        file = open(glob.c.feature__tmp_cfg__file_name, "rb")
         glob.d.feature__tmp_cfg__data = pickle.load(file)
         file.close()
     except FileNotFoundError:
@@ -26,15 +62,23 @@ def feature__tmp_cfg__load_cfg(tmp_cfg_file_name):
 # returns value of loaded temporary configuration
 def feature__tmp_cfg__get_value(name, default = None):
     
+    if glob.d.feature__tmp_cfg__ready == False:
+        print('** FATAL >> feature__tmp_cfg >> must be initialized first!')
+        sys.exit(1)
+    
     ret = glob.d.feature__tmp_cfg__data.get(name, default)
     return ret
     
 # updates value of temporary configuration file
 def feature__tmp_cfg__set_value(name, value, update_tmp_cfg_file = True):
     
+    if glob.d.feature__tmp_cfg__ready == False:
+        print('** FATAL >> feature__tmp_cfg >> must be initialized first!')
+        sys.exit(1)
+    
     glob.d.feature__tmp_cfg__data[name] = value
     
     if update_tmp_cfg_file == True:
-        file = open(glob.d.feature__tmp_cfg__file_name,'wb')
+        file = open(glob.c.feature__tmp_cfg__file_name,'wb')
         pickle.dump(glob.d.feature__tmp_cfg__data, file)
         file.close()
