4f22b6d843431b28851d3f504575d937183351ed
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2026-01-17 19:59:02 +0000

    upt dexbot_v2_proxy.py - update according to pricing outage system feature

--- dexbot_v2_proxy.py
+++ dexbot_v2_proxy.py
@@ -32,7 +32,7 @@ def init_postconfig():
     proxy_server__init_postconfig()
     
     # initialize pricing storage and set proxy client
-    pricing_storage__init_postconfig("proxy.tmp.pricing", 60, "", 2, 6, pricing_proxy_server__pricing_storage__try_get_price_fn)
+    pricing_storage__init_postconfig("proxy.tmp.pricing", 60, 2, 6, pricing_proxy_server__pricing_storage__try_get_price_fn)
 
 def load_config():
     global c, s, d
